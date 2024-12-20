4e06d7d97b0cef33d9d5c1f86e9203be7818a668
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2024-12-20 19:05:28 +0000

    Added AboutWindow

---
+++ AboutWindow.axaml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Enviredit;
+
+public partial class AboutWindow : Window
+{
+    public AboutWindow()
+    {
+        InitializeComponent();
+    }
+}
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -501,4 +501,12 @@ public partial class MainWindow : Window
     {
         refreshHandler.LoadFromList(this);
     }
+
+    private async void About_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var ownerWindow = this;
+        var dialogWindow = new AboutWindow();
+        dialogWindow.RequestedThemeVariant = RequestedThemeVariant;
+        await dialogWindow.ShowDialog(ownerWindow);
+    }
 }
